4030a3052d72ca93247804e84d463f45907bbf34
John C. Burnham <jcb@johnchandlerburnham.com>
2020-09-03 02:57:10 -0400

    Initial draft of Yatima as the untyped λ-calculus
    This commit is based on previous work which uses
    `johnchandlerburnham/yatima` as a framework.
    
    This commit implements:
    
    - Language.Yatima.Term: Definition of Yatima `Term` as the untyped lambda calculus
    - Language.Yatima.Parse: Megaparsec based parsing of `Term`, with errors
    - Language.Yatima.Print: A simple pretty-printer for `Term`
    - Language.Yatima.GHC.Eval: An evaluator for `Term` using GHC native
    lambdas in higher-order-abstract-syntax
    - test.ya: A simple test file showing the syntax
    
    Housekeeping details:
    
    - Initialized `stack` and `hpack` build files.
    - Code from `johnchandlerburnham/yatima` was under AGPLv3, this has been
    changed to GPLv3 via written approval from all contributors

---
+++ src/Language/Yatima/GHC/Eval.hs
@@ -0,0 +1,97 @@
+{-|
+Module      : Language.Yatima.GHC.Eval
+Description : Evaluates exprassions in the Yatima Language using GHC HOAS
+Copyright   : (c) Sunshine Cybernetics, 2020
+License     : GPL-3
+Maintainer  : john@sunshinecybernetics.com
+Stability   : experimental
+-}
+module Language.Yatima.GHC.Eval 
+  ( HOAS(..)
+  , findCtx
+  , toHOAS
+  , fromHOAS
+  , printHOAS
+  , whnf
+  , norm
+  , evalFile
+  , evalPrintFile
+  ) where
+
+import           Data.Text             (Text)
+import qualified Data.Text             as T
+
+import           Language.Yatima.Parse
+import           Language.Yatima.Print
+import           Language.Yatima.Term
+
+-- | Higher-Order Abstract Syntax
+data HOAS where
+  VarH :: Name -> Int -> HOAS
+  LamH :: Name -> (HOAS -> HOAS) -> HOAS
+  AppH :: HOAS -> HOAS -> HOAS
+
+-- | Find a term in a context
+findCtx :: Int -> [HOAS] -> Maybe HOAS
+findCtx i cs = go cs 0
+  where
+    go (c:cs) j
+      | i == j   = Just c
+      | otherwise = go cs (j+1)
+    go [] _     = Nothing
+
+-- | Convert a lower-order `Term` to a GHC higher-order one
+toHOAS :: Term -> [HOAS] -> Int -> HOAS
+toHOAS t ctx dep = case t of
+  Var n i       -> case findCtx i ctx of
+    Just trm -> trm
+    Nothing  -> VarH n (dep - i - 1)
+  Lam n b       -> LamH n  (\x -> bind x b)
+  App f a       -> AppH (go f) (go a)
+  where
+    bind x t = toHOAS t (x:ctx) (dep + 1)
+    go t     = toHOAS t ctx dep
+
+-- | Convert a GHC higher-order representation to a lower-order one
+fromHOAS :: HOAS -> Int -> Term
+fromHOAS t dep = case t of
+  VarH n i   -> Var n (dep - i - 1)
+  LamH n b   -> Lam n (unbind n b)
+  AppH f a   -> App (go f) (go a)
+  where
+    go t       = fromHOAS t dep
+    unbind n b = fromHOAS (b (VarH n dep)) (dep + 1)
+
+-- | Pretty-print a `HOAS`
+printHOAS :: HOAS -> Text
+printHOAS = prettyTerm . (\x -> fromHOAS x 0)
+
+instance Show HOAS where
+  show t = T.unpack $ printHOAS t
+
+-- | Reduce a HOAS to weak-head-normal-form
+whnf :: HOAS -> HOAS
+whnf t = case t of
+  AppH f a       -> case whnf f of
+    LamH _ b -> whnf (b a)
+    x        -> AppH  f a
+  x              -> x
+
+-- | Normalize a HOAS term
+norm :: HOAS -> HOAS
+norm t = case t of
+  LamH n b -> LamH n (\ x -> norm (b x))
+  AppH f a -> AppH (norm f) (norm a)
+  x        -> x
+
+-- | Read and evaluate a `HOAS` from a file
+evalFile :: FilePath -> IO HOAS
+evalFile file = do
+  term <- pFile file
+  return $ norm $ toHOAS term [] 0
+
+-- | Read, eval and print a `HOAS` from a file
+evalPrintFile :: FilePath -> IO ()
+evalPrintFile file = do
+  term <- evalFile file
+  putStrLn $ T.unpack $ printHOAS term
---
+++ src/Language/Yatima/Parse.hs
@@ -0,0 +1,210 @@
+{-|
+Module      : Language.Yatima.Parse
+Description : Parsing expressions in the Yatima Language
+Copyright   : (c) Sunshine Cybernetics, 2020
+License     : GPL-3
+Maintainer  : john@sunshinecybernetics.com
+Stability   : experimental
+-}
+module Language.Yatima.Parse 
+  ( ParseErr(..)
+  , Parser
+  , parseDefault
+  , parserTest
+  , parse'
+  , pName
+  , pLam
+  , pVar
+  , pTerm
+  , pExpr
+  , pFile
+  , prettyFile
+  ) where
+
+import           Control.Monad.Except
+import           Control.Monad.Identity
+import           Control.Monad.RWS.Lazy     hiding (All)
+
+import           Data.Text                  (Text)
+import qualified Data.Text                  as T
+import           Data.Map                   (Map)
+import qualified Data.Map                   as M
+import           Data.Char                  (isDigit)
+import qualified Data.Text.IO               as TIO
+
+import           System.Exit
+
+import           Text.Megaparsec            hiding (State)
+import           Text.Megaparsec.Char       hiding (space)
+import qualified Text.Megaparsec.Char.Lexer as L
+
+import           Language.Yatima.Term
+import           Language.Yatima.Print
+
+
+-- | The environment of a Parser
+data ParseEnv = ParseEnv
+  { -- | The binding context for local variables
+    _context :: [Name]
+  }
+
+-- | An empty parser environment, useful for testing
+defaultParseEnv = ParseEnv []
+
+-- | Custom parser errrors with bespoke messaging
+data ParseErr
+  = UndefinedReference Name
+  | ReservedKeyword Name
+  | LeadingDigit Name
+  | LeadingApostrophe Name
+  deriving (Eq, Ord, Show)
+
+instance ShowErrorComponent ParseErr where
+  showErrorComponent (UndefinedReference nam) =
+    "Undefined reference: " ++ T.unpack nam
+  showErrorComponent (ReservedKeyword nam) =
+    "Reserved keyword: " ++ T.unpack nam
+  showErrorComponent (LeadingDigit nam) = 
+    "illegal leading digit in name: " ++ T.unpack nam
+  showErrorComponent (LeadingApostrophe nam) =
+    "illegal leading apostrophe in name: " ++ T.unpack nam
+
+-- | The type of the Yatima Parser
+type Parser = RWST ParseEnv () () (ParsecT ParseErr Text Identity)
+
+-- | A top level parser with default env and state
+parseDefault :: Show a => Parser a -> Text
+             -> Either (ParseErrorBundle Text ParseErr) a
+parseDefault p s = do
+  (a,_,_) <- runIdentity $ runParserT (runRWST p defaultParseEnv ()) "" s
+  return a
+
+-- | A useful testing function
+parserTest :: Show a => Parser a -> Text -> IO ()
+parserTest p s = case parseDefault p s of
+  Left  e -> putStr (errorBundlePretty e)
+  Right x -> print x
+
+-- | A utility for running a `Parser`, since the `RWST` monad wraps `ParsecT`
+parse' :: Show a => Parser a -> ParseEnv -> String -> Text
+       -> Either (ParseErrorBundle Text ParseErr) a
+parse' p env file txt = do
+  (a,_,_) <- runIdentity $ runParserT (runRWST p env ()) file txt
+  return a
+
+pName :: Parser Text
+pName = label "a name: \"someFunc\",\"somFunc'\",\"x_15\", \"_1\"" $ do
+  n  <- alphaNumChar <|> oneOf nameSymbol
+  ns <- many (alphaNumChar <|> oneOf nameSymbol)
+  let nam = T.pack (n : ns)
+  if | isDigit n                -> customFailure $ LeadingDigit nam
+     | n == '\''                -> customFailure $ LeadingApostrophe nam
+     | nam `elem` reservedWords -> customFailure $ ReservedKeyword nam
+     | otherwise -> return nam
+  where
+    nameSymbol = "_'" :: [Char]
+
+    reservedWords :: [Text]
+    reservedWords = [ "let"
+                    , "if"
+                    , "then"
+                    , "else"
+                    , "where"
+                    , "case"
+                    , "lam"
+                    , "lambda"
+                    , "def"
+                    ]
+
+-- | Consume whitespace, while skipping comments. Yatima line comments begin
+-- with @//@, and block comments are bracketed by @*/@ and @*/@ symbols.
+space :: Parser ()
+space = L.space space1 (L.skipLineComment "--") (L.skipBlockComment "/*" "*/")
+
+-- | A symbol is a string which can be followed by whitespace. The @sc@ argument
+-- is for parsing indentation sensitive whitespace
+symbol :: Text -> Parser Text
+symbol txt = L.symbol space txt
+
+-- | Add a list of names to the binding context
+bind :: [Name] -> Parser a -> Parser a
+bind bs p = local (\e -> e { _context = (reverse bs) ++ _context e }) p
+
+-- | Find a name in the binding context and return its index
+find :: Name -> [Name] -> Maybe Int
+find n cs = go n cs 0
+  where
+    go n (c:cs) i
+      | n == c    = Just i
+      | otherwise = go n cs (i+1)
+    go _ [] _     = Nothing
+
+foldLam:: Term -> [Name] -> Term
+foldLam body bs = foldr (\n x -> Lam n x) body bs
+
+-- | Parse a lambda: @λ (x) (y) (z) => body@
+pLam :: Parser Term
+pLam = label "a lambda: \"λ (x) (y) => y\"" $ do
+  symbol "λ" <|> symbol "lam" <|> symbol "lambda"
+  bs   <- pBinders <* space
+  symbol "=>"
+  body <- bind bs (pExpr False)
+  return $ foldLam body bs
+
+pBinder :: Parser [Name]
+pBinder = label "a single binder in lambda" $ do
+  symbol "("
+  names <- sepEndBy1 pName space
+  string ")"
+  return names
+
+-- | Parse a nonempty binding sequence @(0 A: Type) (1 x : A) (z : C)@
+pBinders :: Parser [Name]
+pBinders = label "a binding sequence in a lambda" $ do
+  (try $ next) <|> pBinder
+  where
+   next = do
+     b  <- pBinder <* space
+     bs <- bind b $ pBinders
+     return $ b ++ bs
+
+-- | Parse a local variable or a locally indexed alias of a global reference
+pVar :: Parser Term
+pVar = label "a local or global reference: \"x\", \"add\"" $ do
+  ctx <- asks _context
+  nam <- pName
+  case find nam ctx of
+    Just i  -> return $ Var nam i
+    Nothing -> customFailure $ UndefinedReference nam
+
+-- | Parse a term
+pTerm :: Parser Term
+pTerm = do
+  choice
+    [ pLam
+    , pExpr True
+    , pVar
+    ]
+
+-- | Parse a sequence of terms as an expression
+pExpr :: Bool -> Parser Term
+pExpr parens = do
+  when parens (void $ symbol "(")
+  fun  <- pTerm <* space
+  args <- sepEndBy (try $ pTerm) space
+  when parens (void $ string ")")
+  return $ foldl (\t a -> App t a) fun args
+
+-- | Parse a file
+pFile :: FilePath -> IO Term
+pFile file = do
+  txt <- TIO.readFile file
+  case parse' (pExpr False) defaultParseEnv file txt of
+    Left  e -> putStr (errorBundlePretty e) >> exitFailure
+    Right m -> return m
+
+-- | Parse and pretty-print a file
+prettyFile :: FilePath -> IO ()
+prettyFile file = do
+  term <- pFile file
+  putStrLn $ T.unpack $ prettyTerm term
